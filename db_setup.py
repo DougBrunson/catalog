33f44235966012e8c725d2c1264e9b2f5da2e1e9
DougBrunson <doug.brunson20@gmail>
2016-09-13 10:37:39 -0400

    added initial data

---
+++ db_setup.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db import Base, Item, User
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+item_1 = Item(title='Nob Hill Studio', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a1.muscache.com/im/pictures/12122/196848ed_original.jpg?aki_policy=x_medium')
+session.add(item_1)
+session.commit()
+
+item_2 = Item(title='Charming Cole Valley Private Suite', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a1.muscache.com/im/pictures/9632106/b0f1c4fa_original.jpg?aki_policy=x_medium')
+session.add(item_2)
+session.commit()
+
+item_3 = Item(title='Stay in the Famous Painted Ladies!', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a2.muscache.com/im/pictures/6c856728-5b88-42bf-94a0-b6484ae1c6f0.jpg?aki_policy=x_medium')
+session.add(item_3)
+session.commit()
+
+item_4 = Item(title='Efficiency Studio in Pacific Height', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a0.muscache.com/im/pictures/dc51020f-4d26-48db-8e36-ad14c8fc5250.jpg?aki_policy=x_medium')
+session.add(item_4)
+session.commit()
+
+item_5 = Item(title='Private Ocean View Retreat w Full Bath', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a1.muscache.com/im/pictures/a607a14a-7938-4ef6-ba6d-1b57bef43d58.jpg?aki_policy=x_medium')
+session.add(item_5)
+session.commit()
+
+item_6 = Item(title='Mission Bay Apartment', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eaque eligendi ullam enim dolore, impedit libero sequi sint maxime atque unde vero voluptatum totam rem vitae molestiae. Consectetur molestiae, ipsum!', img_url='https://a2.muscache.com/im/pictures/081ebc12-a6d0-4b42-bef0-29e7c603f84d.jpg?aki_policy=x_medium')
+session.add(item_6)
+session.commit()
+
