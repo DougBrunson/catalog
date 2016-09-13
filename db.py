81a2cb7a3e25f8b4115ec38714696b642a0c500c
DougBrunson <doug.brunson20@gmail>
2016-09-13 09:34:35 -0400

    added db orm

---
+++ db.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Items(Base):
+    __tablename__ = 'Items'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250))
+    img_url = Column(String(500))
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'img': self.img_url
+        }
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
