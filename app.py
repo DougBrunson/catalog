e71eb86a625158abcac3e9e41d02a8f09fe99227
DougBrunson <doug.brunson20@gmail>
2016-09-13 10:39:48 -0400

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from db import Base, Item
+from db import Base, Item, User
 from flask import session as login_session
 import random
 import string
